ae04915060ac8b456660658ed1c88b0f625b38b9
Glitch (picayune-act) <none>
2018-10-07 09:42:07 +0000

    🦃💋 Checkpoint ./package.json:275522/2676 ./server.js:275522/1757

--- server.js
+++ server.js
@@ -4,11 +4,25 @@
 // init project
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  count: Number,
+  log: [
+    {description: String, duration: Number, date: Date}
+  ],
+});
+
+const User = mongoose.model('User', UserSchema);
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -16,22 +30,6 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
