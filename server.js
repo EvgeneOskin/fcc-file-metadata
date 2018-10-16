105c90598e2f5012dd69b9c84a820df9322f727f
Glitch (picayune-act) <none>
2018-10-16 16:30:35 +0000

    📚🌈 Checkpoint ./views/index.html:275522/1201 ./server.js:275522/3306 ./package.json:275522/945

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true,  dropDups: true  },
-  count: Number,
-  log: [
-    {description: String, duration: Number, date: Date}
-  ],
-});
-const User = mongoose.model('User', UserSchema);
-User.createIndexes()
+const multer = require('multer'); // v1.0.5
+const upload = multer(); // for parsing multipart/form-data
 
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -30,73 +15,16 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-app.post('/api/exercise/new-user', async (req, res) => {
-  const {username } = req.body
-  try {
-    const {_id} = await User.create({ username })
-    res.json({username, id: _id})
-  }catch(e) {
-    res.statusCode = 400
-    res.json({error: 'Cannot create a user'})
-  }
+app.post('/api/fileanalyse', upload.single('file'), async (req, res) => {
+  console.log(req.file);
+  res.json({
+    error: 'Cannot create a user'
+    name:	file.originalname,
+type: file.	"image/jpeg"
+size	71545
+  })
 })
 
-app.post('/api/exercise/add', async (req, res) => {
-  const {
-    userId, description, 
-    duration: durationStr, 
-    date: dateStr=''
-  } = req.body
-  const date = dateStr ? new Date(dateStr) : new Date()
-  const duration = parseInt(durationStr)
-  try {
-    const data = await User.findOneAndUpdate(
-      { _id: userId },
-      {
-        $inc: { count: 1 },
-        $push: {log: {description, duration, date}}
-      },
-      {upsert: true, new: true}
-    )
-    res.json({data})
-  } catch(e) {
-    res.statusCode = 400
-    console.error(e)
-    res.json({error: 'can not create an exsercise'})
-  }
-})
-
-app.get('/api/exercise/log', async (req, res) => {
-  const {
-    userId,
-    from='',
-    to='',
-    limit=null
-  } = req.query
-  try {
-    let query = User.findById(userId)
-    const data = await query.exec()
-    const fromDate = from && new Date(from)
-    const toDate = to && new Date(to)
-    
-    data.log = data.log.filter(({ date }) => {
-      const d = new Date(date)
-      const isValid = (
-        (!fromDate || d - fromDate> 0) &&
-        (!toDate || toDate - d > 0)
-      )
-      return isValid
-    })
-    if (limit) {
-      data.log = data.log.slice(0, parseInt(limit))
-    }
-    res.json(data)
-  } catch(e) {
-    res.statusCode = 400
-    console.error(e)
-    res.json({error: 'can not list logs'})
-  }
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
